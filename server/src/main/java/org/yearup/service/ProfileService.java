b530f5f79926d375fad92ec77f40e5c05da308f2
truc-nt <truc.ntw@gmail.com>
2026-06-21 20:52:09 -0400

    profile: implement get

--- server/src/main/java/org/yearup/service/ProfileService.java
+++ server/src/main/java/org/yearup/service/ProfileService.java
@@ -4,14 +4,19 @@ import org.springframework.stereotype.Service;
 import org.yearup.models.Profile;
 import org.yearup.repository.ProfileRepository;
 
+import java.util.Optional;
+
 @Service
-public class ProfileService
-{
+public class ProfileService {
     private final ProfileRepository profileRepository;
 
     public ProfileService(ProfileRepository profileRepository)
     {
         this.profileRepository = profileRepository;
+    }
+
+    public Optional<Profile> getByUserId(int userId) {
+        return profileRepository.getByUserId(userId);
     }
 
     public Profile create(Profile profile)
