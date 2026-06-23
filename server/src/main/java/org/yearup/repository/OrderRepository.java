f5484671746eb7d3b792d4d58217bb93e349d3e1
truc-nt <truc.ntw@gmail.com>
2026-06-23 02:10:48 -0400

    refactor/cleanup order feature

--- server/src/main/java/org/yearup/repository/OrderRepository.java
+++ server/src/main/java/org/yearup/repository/OrderRepository.java
@@ -2,7 +2,7 @@ package org.yearup.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.yearup.models.Order;
+import org.yearup.model.Order;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
