c5138909f7642dfff9be542d7153df647ac42583
truc-nt <truc.ntw@gmail.com>
2026-06-23 01:37:51 -0400

    refactor/cleanup profile feature

--- server/src/main/java/org/yearup/repository/ProfileRepository.java
+++ server/src/main/java/org/yearup/repository/ProfileRepository.java
@@ -2,7 +2,7 @@ package org.yearup.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.yearup.models.Profile;
+import org.yearup.model.Profile;
 
 import java.util.Optional;
 
